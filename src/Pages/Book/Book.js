63bab4db4b33274a43a5c3eb576bc2643edeb5f7
Daniel-G-Benedict <78005785+Daniel-G-Benedict@users.noreply.github.com>
2022-10-24 06:26:41 -0400

    0.1.2
    Added chat / quiz page

--- src/Pages/Book/Book.js
+++ src/Pages/Book/Book.js
@@ -1,8 +1,41 @@
+ import './Book.css'
+ 
+
+
  const Book = () => {
+   
+// source : https://codepen.io/yoann-b/pen/jOLjjOP
+
    return ( <div>
-        <h1>Hello there!</h1>
-        <h1>I am a book page.</h1>
-        <button onPointerDown={()=> {window.location.replace("./Game")}}> return to game </button>
+
+
+<main>
+	<div className="book">
+		<div className="book-cover">
+			<div>
+				<booktitle>Miranda Rights</booktitle>
+				<div className="separator"></div>
+				<h2>By Dominique Douglas</h2>
+			</div>
+		</div>
+		<div className="book-content">
+			<h3>An Introduction to Miranda Rights</h3>
+
+			<p><b>Miranda rights: </b>The set of rights that a person accused or suspected of having committed a specific offense has during interrogation and of which he or she must be informed prior to questioning, as stated by the U.S. Supreme Court in deciding Miranda v. Arizona and related cases.</p>
+            
+            <p><b>Miranda triggers:</b> The dual principles of custody and interrogation, both of which are necessary before an advisement of rights is required.</p>
+
+            <p><b>Miranda warnings:</b> The advisement of rights due criminal suspects by the police prior to the beginning of questioning. Miranda warnings were first set forth by the U.S. Supreme Court in the 1966 case of Miranda v. Arizona.</p>
+            <p>"You have the right to remain silent. Anything you say can be used against you in court. You have the right to talk to a lawyer for advice before we ask you any questions. You have the right to have a lawyer with you during questioning."</p>
+            
+            <h3>Case Law :  Miranda v. Arizona, 1966</h3>
+            <p> In 1963, Ernesto Miranda was arrested in Phoenix, Arizona for stealing $8 from bank worker and charged with armed robbery. He already had a record for armed robbery, and a juvenile record including attempted rape, assault, and burglary. While in police custody he signed a written confession to the robbery, and to kidnapping and raping an 18-year- old woman 11 days before the robbery. After the conviction, his lawyers appealed, on the grounds that Miranda did not know he was protected from self-incrimination.</p>
+            <p>The case, Miranda v. Arizona, made it all the way to the Supreme Court, where the conviction was overthrown. In a landmark ruling issued in 1966, the court established that the accused have the right to remain silent and that prosecutors may not use statements made by defendants while in police custody unless the police have advised them of their rights, commonly called the Miranda Rights. The case was later re-tried; Miranda was convicted on the basis of other evidence, and served 11 years. He was paroled in 1972, and died in 1976 at the age of 34, after being stabbed in a bar fight. A suspect was arrested but chose to exercise his right to remain silent, and was released.</p>
+
+			</div>
+	</div>
+    <button onPointerDown={()=> {window.location.replace("./Game")}}> return to game </button>
+</main>
     </div>
    )
 }
--- src/Pages/Door/Door.js
+++ src/Pages/Door/Door.js
@@ -1,5 +1,5 @@
- import { useEffect } from 'react';
-//import './Door.css'
+import { useEffect } from 'react';
+import './Door.css'
 
 const Door = () => {
   return(<div>
@@ -10,153 +10,57 @@ const Door = () => {
   )
 }
 
- 
- const Door2 = () => {
+
+const Door2 = () => {
    
-   useEffect(() => {
+// Variables
+var messages = document.getElementById('message-list')
+var btn = document.getElementById('btn')
+var input = document.getElementById('userText')
 
-    return ( 
+// Button/Enter Key
+//btn.addEventListener('click', sendMessage)
+//input.addEventListener('keyup', function(e){ if(e.keyCode == 13) sendMessage() })
 
-      <div>
-<section className="msger">
-   <header className="msger-header">
-     <div className="msger-header-title">
-       <i className="fas fa-comment-alt"></i> SimpleChat
-     </div>
-     <div className="msger-header-options">
-       <span><i className="fas fa-cog"></i></span>
-     </div>
-   </header>
- 
-   <main className="msger-chat">
-     <div className="msg left-msg">
-       <div
-        className="msg-img"
-       
-       ></div>
- 
-       <div className="msg-bubble">
-         <div className="msg-info">
-           <div className="msg-info-name">BOT</div>
-           <div className="msg-info-time">12:45</div>
-         </div>
- 
-         <div className="msg-text">
-           Hi, welcome to SimpleChat! Go ahead and send me a message. 😄
-         </div>
-       </div>
-     </div>
- 
-     <div className="msg right-msg">
-       <div
-        className="msg-img"
-       ></div>
- 
-       <div className="msg-bubble">
-         <div className="msg-info">
-           <div className="msg-info-name">Sajad</div>
-           <div className="msg-info-time">12:46</div>
-         </div>
- 
-         <div className="msg-text">
-           You can change your name in JS section!
-         </div>
-       </div>
-     </div>
-   </main>
- 
-   <form className="msger-inputarea">
-     <input type="text" className="msger-input" placeholder="Enter your message..."/>
-     <button type="submit" className="msger-send-btn">Send</button>
-   </form>
-    </section>
-    <button onPointerDown={()=>{window.location.replace("./Game")}}>return to game</button>
-    </div>
-  )}
-  )
-
-  const msgerForm = get("msger-inputarea");
-  const msgerInput = get(".msger-input");
-  const msgerChat = get(".msger-chat");
-  
-  console.log(msgerForm)
-  
-  const BOT_MSGS = [
-    "Hi, how are you?",
-    "Ohh... I can't understand what you trying to say. Sorry!",
-    "I like to play games... But I don't know how to play!",
-    "Sorry if my answers are not relevant. :))",
-    "I feel sleepy! :("
-  ];
-  
-  // Icons made by Freepik from www.flaticon.com
-  const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
-  const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
-  const BOT_NAME = "BOT";
-  const PERSON_NAME = "Sajad";
-  
-  msgerForm.addEventListener("submit", event => {
-    event.preventDefault();
-  
-    const msgText = msgerInput.value;
-    if (!msgText) return;
-  
-    appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
-    msgerInput.value = "";
-  
-    botResponse();
-  });
-  
-  function appendMessage(name, img, side, text) {
-    //   Simple solution for small apps
-    const msgHTML = `
-      <div class="msg ${side}-msg">
-        <div class="msg-img" style="background-image: url(${img})"></div>
-  
-        <div class="msg-bubble">
-          <div class="msg-info">
-            <div class="msg-info-name">${name}</div>
-            <div class="msg-info-time">${formatDate(new Date())}</div>
-          </div>
-  
-          <div class="msg-text">${text}</div>
-        </div>
-      </div>
-    `;
-  
-    msgerChat.insertAdjacentHTML("beforeend", msgHTML);
-    msgerChat.scrollTop += 500;
-  }
-  
-  function botResponse() {
-    const r = random(0, BOT_MSGS.length - 1);
-    const msgText = BOT_MSGS[r];
-    const delay = msgText.split(" ").length * 100;
-  
-    setTimeout(() => {
-      appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
-    }, delay);
-  }
-  
-  // Utils
-  function get(selector, root = document) {
-    return root.querySelector(selector);
-  }
-  
-  function formatDate(date) {
-    const h = "0" + date.getHours();
-    const m = "0" + date.getMinutes();
-  
-    return `${h.slice(-2)}:${m.slice(-2)}`;
-  }
-  
-  function random(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-
-
+// Messenger Functions
+function sendMessage(string){
+   var msg = string;
+   writeLine(msg)
 }
 
+function writeLine(text){
+   var message = document.createElement('li')
+   message.className = 'player-message';
+   message.innerHTML = 'Ezekial says: ' + text
+   document.getElementById('message-list').appendChild(message)
+   //messages.scrollTop = messages.scrollHeight;
+}
+ 
+function addMessage(e){
+   var msg = e.data ? JSON.parse(e.data) : e;
+   writeLine(`${msg.FROM}: ${msg.MESSAGE}`)
+}
+
+return (
+  <div>
+  <div id="chat">
+    <div id="messages">
+      <ul id="message-list">
+        <li id="officer-message">
+          Officer says: Do you feel lucky?
+        </li>
+      </ul>
+      <div id="message-input">
+        <button type="button" className="btn" onPointerDown={()=>{sendMessage("Option A")}}>Option A</button>
+        <button type="button" className="btn" onPointerDown={()=>{sendMessage("Option B")}}>Option B</button>
+      </div>
+    </div>
+  <button className="btn" onPointerDown={()=> {window.location.replace("./Game")}}> return to game </button>
+  </div>
+</div>
+)
+
+} // close Door2
 
 
-export default Door;
+export default Door2;
